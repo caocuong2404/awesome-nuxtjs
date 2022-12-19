ce7bda9126b1ebc487ee9658a41f34c1bd626f76
Cuong C. Nguyen <cuongnc2404@gmail.com>
2022-12-19 16:07:53 +0700

    feature(ui): add vuetify@latest

--- app.vue
+++ app.vue
@@ -1,5 +1,8 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <v-container>
+      <v-card width="400px" style="margin: 0 auto;" title="Awesome NuxtJS" text="Awesome template for start with NuxtJS"
+        subtitle="NuxtJS@latest + Vuetify@latest" />
+    </v-container>
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  css: [
+    "vuetify/lib/styles/main.sass",
+    "@mdi/font/css/materialdesignicons.min.css",
+  ],
+  build: {
+    transpile: ["vuetify"],
+  },
+  vite: {
+    define: {
+      "process.env.DEBUG": true,
+    },
+  },
   typescript: {
     shim: false,
   },
+  // imports: {
+  //   autoImport: false
+  // }
 });
